7ae21eb69712acd5d67a48a8d916854f63654307
Tales Gomes <tales.santos.gomes@outlook.com>
2025-08-25 17:27:35 -0300

    App locale, number and date formatting

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -4,13 +4,17 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Localization;
 using SalesWebMvc.Media;
 using SalesWebMvc.Data;
 using System;
+using System.Globalization;
 using SalesWebMvc.Services;
+using System.Collections.Generic;
 
 // Cria o builder
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Configura o DbContext com MySQL
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
@@ -27,6 +31,16 @@ builder.Services.AddScoped<DepartmentService>(); // Registra o DepartmentService
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+var enUS = new CultureInfo("en-US");
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(enUS),
+    SupportedCultures = new List<CultureInfo> { enUS },
+    SupportedUICultures = new List<CultureInfo> { enUS }
+};
+
+app.UseRequestLocalization(localizationOptions);
 
 // Executa o seeding de dados dentro de um scope seguro
 using (var scope = app.Services.CreateScope())
